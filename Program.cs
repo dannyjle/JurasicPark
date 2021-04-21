cbfa8ff3c7fe25e1365a454ab287e95f45a12578
Danny Le <le.dannyj@gmail.com>
2021-04-21 14:50:23 -0400

    Class created and moved greeting into a method

--- Program.cs
+++ Program.cs
@@ -12,21 +12,29 @@ namespace JurasicPark
         ///////////// Weight: 
         ///////////// EnclosureNumber:
 
-
-
-
+        public string Name { get; set; }
+        public string DietType { get; set; }
+        public string WhenAcquired { get; set; }
+        public int Weight { get; set; }
+        public int EnclosureNumber { get; set; }
     }
     class Program
     {
-        static void Main(string[] args)
-        {
 
+        static void Greeting()
+        {
             // First we’re creating a greeting/ welcome for “Jurassic Park Database” in code.
             Console.WriteLine();
             Console.WriteLine(new String('*', 40));
             Console.WriteLine("Welcome to the Jurasic Park Database!");
             Console.WriteLine(new String('*', 40));
             Console.WriteLine();
+        }
+
+        static void Main(string[] args)
+        {
+
+            Greeting();
 
 
 
