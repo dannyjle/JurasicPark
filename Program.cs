700cc682c03da83a3dbe1e8366e2a65c9165b185
Danny Le <le.dannyj@gmail.com>
2021-04-21 14:37:38 -0400

    initial commit

--- Program.cs
+++ Program.cs
@@ -2,11 +2,38 @@
 
 namespace JurasicPark
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Dino
     {
-      Console.WriteLine("Welcome to C#");
+        // Then we’re going to create a  Dino Class for pulling information into the list we are going to create that includes 
+        /////////////
+        ///////////// Name: 
+        ///////////// DietType: 
+        ///////////// WhenAcquired: supplied by code (DateTime of DateCreated)
+        ///////////// Weight: 
+        ///////////// EnclosureNumber:
+
+
+
+
     }
-  }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // First we’re creating a greeting/ welcome for “Jurassic Park Database” in code.
+            Console.WriteLine();
+            Console.WriteLine(new String('*', 40));
+            Console.WriteLine("Welcome to the Jurasic Park Database!");
+            Console.WriteLine(new String('*', 40));
+            Console.WriteLine();
+
+
+
+
+
+
+
+        }
+    }
 }
